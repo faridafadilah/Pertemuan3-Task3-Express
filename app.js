f252571f5e5ab6dad776c5b7ff15e38e7e67e863
Farida Fadilah <faridafadilah42807@gmail.com>
2022-09-08 16:20:18 +0700

    Belajar ExpressJS

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'); // Module Express
+const app = express(); 
+const port = 3000; //Port Server
 
+// Url /
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html'); // Menampilkan FIle index.html
 });
-
+// Url /About
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/about.html' );
+    res.sendFile(__dirname + '/about.html' ); // Menampilkan File about.html
 });
-
+// Url selain diatas
 app.use('/', (req, res) => {
     res.status(404);
     res.send('Not Found: 404');
 });
 
+// Membuat Server
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 });
